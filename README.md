b7b0a996c49bb7527c6dad2221a8d61f4708d055
Phong <loveun1988@outlook.com>
2019-07-05 07:14:40 -0500

    Update module for mpps connection

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # RISGW
-this is gateway for RIS
+
+### This app is for listening mpps message gateway on RIS
+
+> RIS system community with dicom server and storage 
+
+> If request patient review, this app will be patient's infomation to dicom device via mpps
+
+> Then complete scan dicom image or another review, dicom device will be mpps message to this app.
+
+> This app will process mpps message then notify this result of patient's progress status and another information to RIS platform
+
+
+
+
+### Requirement and installation
+ - install nodejs
+ - mysql server
+ - runnig mpps service with dcm4che toolkit 
+
+
+
+### Additional feature
+- At this app, checking deplicating user on RIS platform using database session.
+> Be carefull .env config 
+
--- server.js
+++ server.js
@@ -3,10 +3,7 @@ var fs = require('fs'),
 	xmlReader = require('read-xml');
 const https = require('https');
 let m_socket = [];
-const {
-	exec,
-	execSync
-} = require("child_process");
+const { exec, execSync } = require('child_process');
 
 let moment = require('moment');
 
@@ -21,7 +18,7 @@ let io = require('socket.io')(app);
 
 let MPPSPATH = process.cwd() + '\\mpps';
 
-const wmlPath = "H:\\wamp64\\www\\RIS\\assets\\upsxml";
+const wmlPath = 'H:\\wamp64\\www\\RIS\\assets\\upsxml';
 
 var knex = require('knex')({
 	client: 'mysql',
@@ -44,20 +41,25 @@ var options = {
 };
 var parser = require('xml2json');
 
-// check session table and delete unused session. 
+// check session table and delete unused session.
 
 function checkSessionTable() {
-	knex.select('*').from('ci_sessions').then(function (rows) {
-		for (var key in rows) {
-			if (CompareTimeOut(rows[key])) {
-				DeleteTableData(rows[key].id)
-			} else {}
-		}
-	}).catch(function (error) {
-		console.error(error);
-	})
+	knex
+		.select('*')
+		.from('ci_sessions')
+		.then(function(rows) {
+			for (var key in rows) {
+				if (CompareTimeOut(rows[key])) {
+					DeleteTableData(rows[key].id);
+				} else {
+				}
+			}
+		})
+		.catch(function(error) {
+			console.error(error);
+		});
 }
-// update session latest time 
+// update session latest time
 function CompareTimeOut(currtimeStamp) {
 	// console.log(Math.round(Date.now()/1000) - currtimeStamp.timestamp);
 	if (Date.now() / 1000 - currtimeStamp.timestamp > 200) {
@@ -65,143 +67,144 @@ function CompareTimeOut(currtimeStamp) {
 	} else {
 		return false;
 	}
-
 }
 // check session time over and delete session record at session table
 
 function DeleteTableData(session_id) {
-	knex('ci_sessions').where('id', session_id).del().then(function (result) {
-		console.log(session_id + "   is deleted")
+	knex('ci_sessions').where('id', session_id).del().then(function(result) {
+		console.log(session_id + '   is deleted');
 	});
 }
 
 //set worklist server
 
-
 function startDcmOf() {
-	var command = "dcmof -mwl " + wmlPath + " -mppsxml " + MPPSPATH + " -c dcmof:11116";
-    // var command = "dcmof -mwl " + wmlPath + " dcmof:11116";
+	var command = 'dcmof -mwl ' + wmlPath + ' -mppsxml ' + MPPSPATH + ' -c dcmof:11116';
+	// var command = "dcmof -mwl " + wmlPath + " dcmof:11116";
 	console.log(command);
 	const child = exec(command, (error, stdout, stderr) => {
 		if (error) {
-			console.log("this is error", error);
+			console.log('this is error', error);
 		}
 		console.log(stdout);
 	});
-
 }
 
 // update mpps status
 
 async function updateMpps(params) {
-
 	var checkQuery = {
 		mpps_accession_number: params.mpps_accession_number
 	};
 	console.log(checkQuery);
-	if(!params.hasOwnProperty('mpps_accession_number')){
+	if (!params.hasOwnProperty('mpps_accession_number')) {
 		return true;
 	}
-	knex.select('*').from('tbl_mpps').where(checkQuery).then((rows) => {
-		// console.log('this is tbl_mpps', rows);
-		if (rows.length > 0) {
-			// console.log(params);
-			knex('tbl_mpps').where(checkQuery).update(params).then((data) => {
-				var updateWhere = {
-					chc_id: params.mpps_accession_number
-				};
-				var status = 0;
-				if (params.mpps_status == 'IN PROGRESS') {
-					status = 1;
-				} else if (params.mpps_status == 'COMPLETED') {
-					status = 2;
-				}
-				var updateList = {
-					checkup_status: status
-				};
+	knex
+		.select('*')
+		.from('tbl_mpps')
+		.where(checkQuery)
+		.then((rows) => {
+			// console.log('this is tbl_mpps', rows);
+			if (rows.length > 0) {
+				// console.log(params);
+				knex('tbl_mpps').where(checkQuery).update(params).then((data) => {
+					var updateWhere = {
+						chc_id: params.mpps_accession_number
+					};
+					var status = 0;
+					if (params.mpps_status == 'IN PROGRESS') {
+						status = 1;
+					} else if (params.mpps_status == 'COMPLETED') {
+						status = 2;
+					}
+					var updateList = {
+						checkup_status: status
+					};
 
-				knex('tbl_check_list').where(updateWhere).update(updateList).then(data => {
-					// console.log('update is 1');
+					knex('tbl_check_list').where(updateWhere).update(updateList).then((data) => {
+						// console.log('update is 1');
 
-					io.emit('updateRoomStatus', {
-						chc_id: params.mpps_accession_number,
-						status: status
+						io.emit('updateRoomStatus', {
+							chc_id: params.mpps_accession_number,
+							status: status
+						});
+						updateBookingStatus(updateWhere);
 					});
-					updateBookingStatus(updateWhere);
-
 				});
-			});
-
-		} else {
-			// console.log(params);
-			knex('tbl_mpps').returning('id').insert(params).then((data) => {
-				var updateWhere = {
-					chc_id: params.mpps_accession_number
-				};
-				var status = 0;
-				if (params.mpps_status == 'IN PROGRESS') {
-					status = 1;
-				} else if (params.mpps_status == 'COMPLETED') {
-					status = 2;
-				}
-				var updateList = {
-					checkup_status: status
-				};
-				knex('tbl_check_list').where(updateWhere).update(updateList).then(data => {
-					io.emit('updateRoomStatus', {
-						chc_id: params.mpps_accession_number,
-						status: status
+			} else {
+				// console.log(params);
+				knex('tbl_mpps').returning('id').insert(params).then((data) => {
+					var updateWhere = {
+						chc_id: params.mpps_accession_number
+					};
+					var status = 0;
+					if (params.mpps_status == 'IN PROGRESS') {
+						status = 1;
+					} else if (params.mpps_status == 'COMPLETED') {
+						status = 2;
+					}
+					var updateList = {
+						checkup_status: status
+					};
+					knex('tbl_check_list').where(updateWhere).update(updateList).then((data) => {
+						io.emit('updateRoomStatus', {
+							chc_id: params.mpps_accession_number,
+							status: status
+						});
+						updateBookingStatus(updateWhere);
 					});
-					updateBookingStatus(updateWhere);
+					// console.log(data);
 				});
-				// console.log(data);
-			});
-		}
-	}).catch(function (error) {
-		console.error(error);
-	});
+			}
+		})
+		.catch(function(error) {
+			console.error(error);
+		});
 }
 
 // update booking status
-
 
 function updateBookingStatus(params) {
 	io.emit('notificationMpps', {
 		chc_id: params.chc_id
 	});
 	console.log('this is notification', params);
-	knex('tbl_check_list').where(params).select().then(getbkId => {
+	knex('tbl_check_list').where(params).select().then((getbkId) => {
 		// console.log(getbkId);
 		if (getbkId.length < 1) {
 			return;
 		}
 		console.log('this is ', getbkId[0]['chc_booking_id']);
 		var booking_id = getbkId[0]['chc_booking_id'];
-		knex('tbl_check_list').where({
-			chc_booking_id: booking_id
-		}).select('checkup_status').then(checklistbybkid => {
-			var isCheckup = 0;
-			for (var key in checklistbybkid) {
-				// console.log('this is checkupStatus', checklistbybkid[key].checkup_status);
-				if (checklistbybkid[key].checkup_status == 2) {
-					isCheckup++;
-				} else {
-
+		knex('tbl_check_list')
+			.where({
+				chc_booking_id: booking_id
+			})
+			.select('checkup_status')
+			.then((checklistbybkid) => {
+				var isCheckup = 0;
+				for (var key in checklistbybkid) {
+					// console.log('this is checkupStatus', checklistbybkid[key].checkup_status);
+					if (checklistbybkid[key].checkup_status == 2) {
+						isCheckup++;
+					} else {
+					}
 				}
-
-			}
-			if (isCheckup == checklistbybkid.length) {
-				knex('tbl_patient_booking').where({
-					booking_id: booking_id
-				}).update({
-					booking_status: 2,
-					checked_time: moment().format('YYYY-MM-DD h:mm:ss')
-				}).then(result => {
-					console.log('update', result);
-				})
-			}
-
-		})
+				if (isCheckup == checklistbybkid.length) {
+					knex('tbl_patient_booking')
+						.where({
+							booking_id: booking_id
+						})
+						.update({
+							booking_status: 2,
+							checked_time: moment().format('YYYY-MM-DD h:mm:ss')
+						})
+						.then((result) => {
+							console.log('update', result);
+						});
+				}
+			});
 	});
 }
 
@@ -219,22 +222,20 @@ function parsingMPPS() {
 	});
 }
 
-
 function scanMpps() {
 	fs.watch(MPPSPATH, (eventType, filename) => {
 		console.log(`event type is: ${eventType}`);
 		if (eventType == 'change') {
 			var filePath = MPPSPATH + '\\' + filename;
-			setTimeout(function () {
+			setTimeout(function() {
 				xmlParserToJson(filePath);
 			}, 3000);
 		}
-
 	});
 }
 
 function xmlParserToJson(params) {
-	xmlReader.readXML(fs.readFileSync(params), function (err, data) {
+	xmlReader.readXML(fs.readFileSync(params), function(err, data) {
 		if (err) {
 			console.error(err);
 		}
@@ -244,8 +245,8 @@ function xmlParserToJson(params) {
 		for (var key in atrrMpps) {
 			switch (atrrMpps[key].tag) {
 				case '00400270':
-					for(var index in atrrMpps[key].item.attr){
-						if(atrrMpps[key].item.attr[index].tag == '00080050') {
+					for (var index in atrrMpps[key].item.attr) {
+						if (atrrMpps[key].item.attr[index].tag == '00080050') {
 							updateDate.mpps_accession_number = atrrMpps[key].item.attr[index]['$t'];
 						}
 					}
@@ -273,63 +274,65 @@ function delectWML(params) {
 		if (err) return;
 		console.log('successfully deleted', params);
 	});
-
 }
-io.on('connection', function (socket) {
+io.on('connection', function(socket) {
 	var addedUser = false;
-	socket.on('connectThisRoom', data => {
-		console.log('this is connect room', data)
+	socket.on('connectThisRoom', (data) => {
+		console.log('this is connect room', data);
 		socket.deviceId = data.device_id;
 		socket.device_doc_name = data.device_doc_name;
 		let updateData = {
 			device_doc_name: data.device_doc_name,
 			room_status: 1
-		}
-		knex('tbl_device').where({
-			id: data.device_id
-		}).update(updateData).then(data => {
-			console.log(data);
-		});
-
+		};
+		knex('tbl_device')
+			.where({
+				id: data.device_id
+			})
+			.update(updateData)
+			.then((data) => {
+				console.log(data);
+			});
 	});
 
 	socket.on('openLessionRoom', (params) => {
 		let updateData = {
 			lession_status: 1
-		}
+		};
 		let where = {
 			lession_id: params.lession_id
-		}
-		knex('tbl_lession_info').where(where).update(updateData).then(data => {
-			io.emit('notificationLession', {lession_id: params.lession_id, lession_status: 1});
+		};
+		knex('tbl_lession_info').where(where).update(updateData).then((data) => {
+			io.emit('notificationLession', { lession_id: params.lession_id, lession_status: 1 });
 			console.log(data);
 		});
 		socket.lession_id = params.lession_id;
 	});
 	// when the user disconnects.. perform this
-	socket.on('disconnect', function () {
+	socket.on('disconnect', function() {
 		if (socket.hasOwnProperty('deviceId')) {
 			let updateData = {
 				device_doc_name: '',
 				room_status: 0
-			}
-			knex('tbl_device').where({
-				id: socket.deviceId
-			}).update(updateData).then(data => {
-				console.log(data);
-			});
-
-		}else if(socket.hasOwnProperty('lession_id')){
+			};
+			knex('tbl_device')
+				.where({
+					id: socket.deviceId
+				})
+				.update(updateData)
+				.then((data) => {
+					console.log(data);
+				});
+		} else if (socket.hasOwnProperty('lession_id')) {
 			let updateData = {
 				lession_status: 0
-			}
+			};
 			let where = {
 				lession_id: socket.lession_id
-			}
-			knex('tbl_lession_info').where(where).update(updateData).then(data => {
+			};
+			knex('tbl_lession_info').where(where).update(updateData).then((data) => {
 				console.log(data);
-				io.emit('notificationLession', {lession_id: socket.lession_id, lession_status: 0});
-
+				io.emit('notificationLession', { lession_id: socket.lession_id, lession_status: 0 });
 			});
 		}
 	});
